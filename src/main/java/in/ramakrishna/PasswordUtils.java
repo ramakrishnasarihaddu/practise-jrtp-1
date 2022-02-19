36bd4d2c52621dd45081fec102e3518253e0abbb
ramakrishnasarihaddu <44487314+ramakrishnasarihaddu@users.noreply.github.com>
2022-02-19 19:18:25 +0530

    Update PasswordUtils.java
    Password changes in

--- src/main/java/in/ramakrishna/PasswordUtils.java
+++ src/main/java/in/ramakrishna/PasswordUtils.java
@@ -4,6 +4,11 @@ public class PasswordUtils {
 	
 	public void encode() {
 		
+        System.out.println( "sAMPLE jENKINS" );
+        System.out.println("IT WILL BE TAKEN");
+        System.out.println("sAMPLE TO GATHERING");
+
+		
 	}
 	
 	public void decode() {
