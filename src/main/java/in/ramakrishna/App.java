8ca14007d05e882af0c1a52c3896757ca7cfc742
ramakrishnasarihaddu <ramakrishnasri02@gmail.com>
2022-02-19 11:29:00 +0530

    samplecommit

---
+++ src/main/java/in/ramakrishna/App.java
@@ -0,0 +1,14 @@
+package in.ramakrishna;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Sample Process");
+    }
+}
